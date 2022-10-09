e17da6076f7e49448a53a13011b6d29155328d70
htmono <heikkimononen@gmail.com>
2022-10-09 20:37:10 +0300

    Added system memory.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import platform
 import os
 import psutil
 import time
+import shutil
+
 
 
 # Yksinkertainen fetch jolla saada terminaaliin näkyviin koneen speksit sekä muuta tietoa.
@@ -11,11 +13,12 @@ import time
 # Lisänä ASCII-art ja muotoilu?
 
 
+
 uname = platform.uname()
 print(f"System: {uname.system} {platform.architecture()[0]} {uname.version.split()[0]}")
 print(f"Hostname: {uname.node}")
 print(f"Kernel Release: {uname.release}")
-print(f"Machine: {uname.machine}")
-print(f"Processor: {uname.processor}","with",os.cpu_count(),"cores")
+print(f"Machine: {uname.machine}") # Placeholder
+print(f"Processor: {uname.processor}","with",os.cpu_count(),"cores") # Placeholder
 print(f"Uptime: {round((time.time() - psutil.boot_time())) // 60}min")
-
+print(f"Memory: {round(psutil.virtual_memory().total / 1024 **2)} MiB")
